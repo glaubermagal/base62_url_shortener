1abffe592650ccedc817a29c3406eb641bd088d2
Glauber Magalhães <glauber.tecinfo@gmail.com>
2023-12-20 23:19:29 -0300

    refactor from base64 to base62

--- apps/urls/tests.py
+++ apps/urls/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from .utils import decimal_to_base66
+from .utils import decimal_to_base62
 
-class Base66ConversionTest(TestCase):
-    def test_decimal_to_base66(self):
+class Base62ConversionTest(TestCase):
+    def test_decimal_to_base62(self):
         test_cases = [
             (0, '0'),
             (12345, '2t3'),
@@ -10,9 +10,7 @@ class Base66ConversionTest(TestCase):
             (1130000000, 'xaWK8'), # There are about 1.13 billion websites on the internet in 2023. Source: https://www.forbes.com/advisor/business/software/website-statistics/
         ]
 
-        for decimal_num, expected_base66 in test_cases:
-            with self.subTest(decimal_num=decimal_num, expected_base66=expected_base66):
-                result = decimal_to_base66(decimal_num)
-                self.assertEqual(result, expected_base66)
-
-                
+        for decimal_num, expected_base62 in test_cases:
+            with self.subTest(decimal_num=decimal_num, expected_base62=expected_base62):
+                result = decimal_to_base62(decimal_num)
+                self.assertEqual(result, expected_base62)
--- apps/urls/utils.py
+++ apps/urls/utils.py
@@ -1,10 +1,10 @@
-def decimal_to_base66(decimal_num):
-    characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_.~"
-    base66_num = ""
+def decimal_to_base62(decimal_num):
+    characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    base62_num = ""
 
     while decimal_num > 0:
         remainder = decimal_num % 66
-        base66_num = characters[remainder] + base66_num
+        base62_num = characters[remainder] + base62_num
         decimal_num //= 66
 
-    return base66_num if base66_num else "0"
+    return base62_num if base62_num else "0"
